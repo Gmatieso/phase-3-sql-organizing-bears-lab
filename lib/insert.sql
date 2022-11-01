f7065baf7df5f29790a5c732aa7f2f6932dd2fb1
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-01 16:17:00 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO  bears (name,age,sex,color,temperament,alive ) VALUES ("Mr. Chocolate",6,"M","white brown","LOYALITY",1);
+INSERT INTO  bears (name,age,sex,color,temperament,alive ) VALUES ("Rowdy",9,"F","brown","PLAYFUL",0);
+INSERT INTO  bears (name,age,sex,color,temperament,alive ) VALUES ("Tabitha",8,"M","white","LOVING",1);
+INSERT INTO  bears (name,age,sex,color,temperament,alive ) VALUES ("Sergeant Brown",5,"M","black","DANGEROUS",1);
+INSERT INTO  bears (name,age,sex,color,temperament,alive ) VALUES ("Melissa",6,"F","dark brown","VIGILANT",0);
+INSERT INTO  bears (name,age,sex,color,temperament,alive ) VALUES ("Grinch",8,"M","dark grey","FURIOUS",0);
+INSERT INTO  bears (name,age,sex,color,temperament,alive ) VALUES ("Wendy",4,"F","black","CARING",1);
+INSERT INTO  bears (name) VALUES ( NULL);
+
